b7549b30859e0e15eb247fe860164c6d0a6da463
PRIYARANJAN JENA <jena.priya13@gmail.com>
2024-12-27 16:23:24 +0000

    ckndck

---
+++ index.ts
@@ -0,0 +1,8 @@
+// function to convert string to any case i mention
+
+function stringConverter(str: string): string {
+    return str.toUpperCase();
+}
+
+console.log(stringConverter("hello world")); // HELLO WORLD
+console.log(stringConverter("hello world")); // Invalid option! Please provide a valid option.
