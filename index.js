5783b3da1079419272c12d1c5b23b96197438a10
priyaranjanjen <jena.priya13@gmail.com>
2024-12-27 22:26:54 +0530

    ccscscs

---
+++ index.js
@@ -0,0 +1,9 @@
+// // function to convert string to any case i mention
+// function stringConverter(str) {
+//     return str.toUpperCase();
+// }
+// console.log(stringConverter("hello world")); // HELLO WORLD
+
+import {toLowercase} from 'string-lowercase';
+
+console.log(toLowercase("PRIYARANJAN"));
---
+++ node_modules/string-lowercase/test.ts
@@ -0,0 +1,4 @@
+import { toLowercase } from './index';
+
+console.log(toLowercase('HELLO WORLD!')); // hello world!
+console.log(toLowercase('TypeScript IS Fun!')); // typescript is fun!
